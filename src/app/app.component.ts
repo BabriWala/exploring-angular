c286cc9b46442402999bbf4f354b1b347c4369f2
babriwala <hanzalafarhann@gmail.com>
2023-08-24 10:43:28 +0600

    My First Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>Hello I am from inline Template</h1>
+  `,
+  // styleUrls: ['./app.component.scss']
+  styles: [`h1{background-color: red}`]
 })
 export class AppComponent {
   title = 'hotelInventory';
